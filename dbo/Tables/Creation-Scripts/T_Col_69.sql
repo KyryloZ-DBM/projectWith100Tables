78d39d1d6e1ae976f84e02be815c218e81f7b8e6
KyryloZ-DBM <email>
2021-10-12 07:24:39 +0000

    add 100 table

---
+++ dbo/Tables/Creation-Scripts/T_Col_69.sql
@@ -0,0 +1,1 @@
+CREATE TABLE [dbo].[T_Col_69](	  [Column1] INT NOT NULL IDENTITY(1         ,1)	, [Column2] NVARCHAR(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL	, CONSTRAINT [PK_T_2Col_69_Column1] PRIMARY KEY ([Column1] ASC))GO
