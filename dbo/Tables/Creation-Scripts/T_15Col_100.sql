4fb9493553391b4ff650320b8a782373357d38c7
KyryloZhdanov <email>
2022-01-17 07:22:12 +0000

    add new func and table

---
+++ dbo/Tables/Creation-Scripts/T_15Col_100.sql
@@ -0,0 +1,27 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[T_15Col_100](
+	[Column1] [int] IDENTITY(1,1) NOT NULL,
+	[Column2] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column3] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column4] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column5] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column6] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column7] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column8] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column9] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column10] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column11] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column12] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column13] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column14] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Column15] [nvarchar](50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+ CONSTRAINT [PK_T_15Col_100_Column1] PRIMARY KEY CLUSTERED 
+(
+	[Column1] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF)
+)
+
+GO
